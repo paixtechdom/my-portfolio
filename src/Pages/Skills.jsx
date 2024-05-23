a412b59d440ea537aa4b54d0b11faa12ac4ae064
paixtechdom <paixtechdom@gmail.com>
2024-05-23 11:10:47 +0100

    adjusted parallax id error

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -76,7 +76,7 @@ export const Skills = () => {
         <div className=" w-11/12 md:w-10/12 lg:w-9/12 grid flex-wrap grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 text-gray-100 gap-4">
             {
                 myskills.map((skill, i) => (
-                    <Parallax key={i} id={skill.name.replaceAll(' ', '')} className={`${i == myskills.length - 1 ? 'col-span-3 md:col-span-2' : ''}`}>
+                    <Parallax key={i} id={skill.name.replaceAll(' ', '').replaceAll('.','')} className={`${i == myskills.length - 1 ? 'col-span-3 md:col-span-2' : ''}`}>
 
                         <div className={`shadow center text-center p-3 bg-gray-900 rounded-xl text-sm font-bold border border-purple-900 cursor-pointer transition-all duration-1000 hover:scale-90 active:scale-90 hover:animate-bounce `}>
                             {skill.name}
