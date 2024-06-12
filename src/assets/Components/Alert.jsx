f977a1d9f5c236bb3873f771000aea4f8e20bc8c
paixtechdom <paixtechdom@gmail.com>
2024-06-12 23:40:30 +0100

    adjusted alert width for mobile

--- src/assets/Components/Alert.jsx
+++ src/assets/Components/Alert.jsx
@@ -14,9 +14,9 @@ export const Alert = () =>{
             <div className={` rounded-xl h-96 w-11/12 md:w-7/12 lg:w-5/12 center flex-col border gap-5  bg-black ${alertType == 'error' ? 'border-red-400' : 'border-green-400'} text-gray-100`}
             >
                 <i className={`bi bi-${alertType == 'error' ? 'x-circle-fill text-red-500' : 'check-circle-fill text-green-700'}  text-7xl mb-4`}></i>
-                <h3 className='text-center font-bold text-gray-200 text-3xl'>{alertMessage}</h3>
+                <h3 className='text-center font-bold text-gray-200 text-2xl w-9/12 md:w-full lg:text-3xl'>{alertMessage}</h3>
 
-                <button className='bg-dark bg-gray-900 border w-4/12 px-7 border-gray-100 rounded-xl p-2 mt-6' 
+                <button className='bg-dark bg-gray-900 border w-6/12 lg:w-5/12 px-7 border-gray-100 rounded-xl p-2 mt-6' 
                 onClick={() => {setShowAlert(false)
                     document.body.style.overflowY = 'scroll'
 
